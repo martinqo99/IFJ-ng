f1e2e8b1267e4e2a498601b11a12e50a1c9b9f93
xkolac12 <xkolac12@stud.fit.vutbr.cz>
2013-10-12 22:13:51 +0200

    Predpripravena kostra pro scanner a strings
    Signed-off-by: xkolac12 <xkolac12@stud.fit.vutbr.cz>

---
+++ scanner.c
@@ -0,0 +1,21 @@
+/**
+ * Predmet:  IFJ / IAL
+ * Projekt:  Implementace interpretu jazyka PHP13
+ * Varianta: a/1/I
+ * Soubor:   scanner.c
+ *
+ * Popis:
+ *
+ *
+ * Datum:    12.9.2013
+ *
+ * Autori:   Frantisek Kolacek   <xkolac12@stud.fit.vutbr.cz>
+ *           Stodulka Daniel
+ *           Hermann Lukas
+ *           Tran Manh Hoang
+ */
+
+#include "scanner.h"
+
+FILE* gFileHandler = NULL;
+tToken gToken;
