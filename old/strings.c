ae6106d11f05b5ab6d35405f6d251f48fff5e22a
xkolac12 <xkolac12@stud.fit.vutbr.cz>
2013-10-27 12:45:16 +0100

    Pridani stringu

---
+++ old/strings.c
@@ -0,0 +1,18 @@
+/**
+ * Predmet:  IFJ / IAL
+ * Projekt:  Implementace interpretu jazyka PHP13
+ * Varianta: a/1/I
+ * Soubor:   strings.c
+ *
+ * Popis:
+ *
+ *
+ * Datum:    12.9.2013
+ *
+ * Autori:   Frantisek Kolacek   <xkolac12@stud.fit.vutbr.cz>
+ *           Stodulka Daniel
+ *           Hermann Lukas
+ *           Tran Manh Hoang
+ */
+
+#include "strings.h"
