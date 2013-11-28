053a6472e04e4612411b7774ef0d8c63bb7a8985
xkolac12 <xkolac12@stud.fit.vutbr.cz>
2013-11-28 20:34:17 +0100

    Nova implementace stacku

--- stack.c
+++ stack.c
@@ -1,30 +1,86 @@
+/**
+ * Predmet:  IFJ / IAL
+ * Projekt:  Implementace interpretu jazyka PHP13
+ * Varianta: a/1/I
+ * Soubor:   stack.c
+ *
+ * Popis:
+ *
+ *
+ * Datum:    28.11.2013
+ *
+ * Autori:   Frantisek Kolacek   <xkolac12@stud.fit.vutbr.cz>
+ *           Stodulka Daniel
+ *           Hermann Lukas
+ *           Tran Manh Hoang
+ */
+
 #include "stack.h"
 
-
-void Sinit(TStack* s){
-    s->SPole  = malloc( MAX_SIZE * sizeof(void *) );
-    s->Top = s->SPole;
+void stackInit(STACK_PTR stack){
+	if(!stack)
+		return;
+	
+	stack->begin = NULL;
+	stack->count = 0;
 }
 
-void SPush(TStack* s, void* EL){
-        s->Top  = EL;
-        (s->Top)++;
+void stackPush(STACK_PTR stack, PTR value){
+	if(!stack)
+		return;	
+	
+	STACK_NODE_PTR tmp = malloc(sizeof(STACK_NODE));
+	
+	tmp->value = value;
+	tmp->next = stack->begin;
+	
+	stack->begin = tmp;
 }
 
-void* SPop(TStack* s){
-    void* tmp;
-    if(s->Top > *s->SPole){
-      tmp = s->Top;
-      (s->Top)--;
-    }
-    return tmp;
+PTR stackPop(STACK_PTR stack){
+	if(!stack)
+		return NULL;
+	
+	if(!stack->begin)
+		return NULL;
+	
+	STACK_NODE_PTR tmp = stack->begin;
+	PTR value = tmp->value;
+	stack->begin = tmp->next;
+
+	free(tmp);	
+	
+	return value;
 }
 
-int SEmpty(TStack* s){
+PTR stackBegin(STACK_PTR stack){
+	if(!stack)
+		return NULL;
+	
+	if(!stack->begin)
+		return NULL;
 
-    s->Top = NULL;
+	return stack->begin->value;
+}
 
-    free(s->SPole);
-    s->SPole = NULL;
-    return 1;
+int stackCount(STACK_PTR stack){
+	if(!stack)
+		return 0;	
+	
+	return stack->count;
+}
+
+void stackFree(STACK_PTR stack){
+	if(!stack)
+		return;	
+
+    STACK_NODE_PTR tmp;
+	
+	while((tmp = stack->begin)){
+		stack->begin = tmp->next;
+		
+		free(tmp);
+	}	
+	
+	stackInit(stack);
 }
--- stack.h
+++ stack.h
@@ -1,26 +1,48 @@
+/**
+ * Predmet:  IFJ / IAL
+ * Projekt:  Implementace interpretu jazyka PHP13
+ * Varianta: a/1/I
+ * Soubor:   stack.h
+ *
+ * Popis:
+ *
+ *
+ * Datum:    28.11.2013
+ *
+ * Autori:   Frantisek Kolacek   <xkolac12@stud.fit.vutbr.cz>
+ *           Stodulka Daniel
+ *           Hermann Lukas
+ *           Tran Manh Hoang
+ */
+
+#ifndef TYPE_PTR_DEFINED
+#define TYPE_PTR_DEFINED
+typedef void* PTR;
+#endif
+
 #ifndef STACK_H_INCLUDED
 #define STACK_H_INCLUDED
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct STACK_NODE{
+	PTR value;
+	struct STACK_NODE* next;		
+} STACK_NODE, *STACK_NODE_PTR;
+
+typedef struct STACK{
+	STACK_NODE_PTR begin;
+	int count;	
+} STACK, *STACK_PTR;
 
 
-#define MAX_SIZE 255
+void stackInit(STACK_PTR stack);
+void stackPush(STACK_PTR stack, PTR value);
+PTR stackPop(STACK_PTR stack);
+PTR stackBegin(STACK_PTR stack);
+int stackCount(STACK_PTR stack);
+void stackFree(STACK_PTR stack);
 
 
-typedef struct{
-    void **SPole;
-    void* Top;
-}TStack;
-
-
-
-void Sinit(TStack* s);
-void SPush(TStack* s,void* El);
-void* SPop(TStack* s);
-int SEmpty(TStack* s);
-
-
-
-
-#endif // STACK_H_INCLUDED
+#endif
